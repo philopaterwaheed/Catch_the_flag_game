aa9f0e58a7039244dfb0a3e35bb55f97f59102fb
philosan <you@example.com>
2023-12-04 23:10:29 +0200

    a stable version of every thing

--- catch_the_flag/src/eventListener.java
+++ catch_the_flag/src/eventListener.java
@@ -11,9 +11,9 @@ import java.util.ArrayList;
 
 public class eventListener  extends AnimListener implements GLEventListener{
     int frame = 0 ;
-    //String textureNames[] = {"flag//flag animation1.png","flag//flag animation2.png","flag//flag animation3.png","flag//flag animation4.png","flag//flag animation5.png"};
-   // TextureReader.Texture texture[] = new TextureReader.Texture[textureNames.length];
-    //int textures[] = new int[textureNames.length];
+    String textureNames[] = {"flag//flag animation1.png","flag//flag animation2.png","flag//flag animation3.png","flag//flag animation4.png","flag//flag animation5.png"};
+    TextureReader.Texture texture[] = new TextureReader.Texture[textureNames.length];
+    int textures[] = new int[textureNames.length];
     Player player1  ;
     static GL gl ;
     @Override
@@ -26,36 +26,64 @@ public class eventListener  extends AnimListener implements GLEventListener{
         gl.glOrtho(0.0, 100.0, 0.0, 100.0, -1, 1); // is not need to easy
         gl.glEnable(GL.GL_TEXTURE_2D);  // Enable Texture Mapping
         gl.glBlendFunc(GL.GL_SRC_ALPHA, GL.GL_ONE_MINUS_SRC_ALPHA);
-
-        player1 = new Player(1 , 1 , true  , new String[]{"flag//flag animation1.png","flag//flag animation2.png","flag//flag animation3.png","flag//flag animation4.png","flag//flag animation5.png"}) ;
-        entityManager.addEntity(player1);
-
-        for (Entity entity : entityManager.entities) {
-
-            for(int i = 0; i < entity.texture.length; i++) {
+        gl.glGenTextures(textureNames.length, textures, 0);
 
 
-                try {
-                    entity.texture [i] = TextureReader.readTexture(assetsFolderName + "//" + entity.Textures.get(i), true);
-                    gl.glBindTexture(GL.GL_TEXTURE_2D, entity.textures[i] );
 
-    //                mipmapsFromPNG(gl, new GLU(), texture[i]);
-                    new GLU().gluBuild2DMipmaps(
-                            GL.GL_TEXTURE_2D,
-                            GL.GL_RGBA, // Internal Texel Format,
-                            entity.texture[i].getWidth(),entity.texture[i].getHeight(),
-                            GL.GL_RGBA, // External format from image,
-                            GL.GL_UNSIGNED_BYTE,
-                            entity.texture[i].getPixels() // Image_data
-                    );
-                } catch (IOException e) {
-                    System.out.println(e.getMessage());
-                    e.printStackTrace();
-                }
+        for (int i = 0; i < textureNames.length; i++) {
+
+
+            try {
+                texture[i] = TextureReader.readTexture(assetsFolderName + "//" + textureNames[i], true);
+                gl.glBindTexture(GL.GL_TEXTURE_2D, textures[i]);
+
+//                mipmapsFromPNG(gl, new GLU(), texture[i]);
+                new GLU().gluBuild2DMipmaps(
+                        GL.GL_TEXTURE_2D,
+                        GL.GL_RGBA, // Internal Texel Format,
+                        texture[i].getWidth(), texture[i].getHeight(),
+                        GL.GL_RGBA, // External format from image,
+                        GL.GL_UNSIGNED_BYTE,
+                        texture[i].getPixels() // Imagedata
+                );
+            } catch (IOException e) {
+                System.out.println(e);
+                e.printStackTrace();
             }
         }
-//        DrawBackground(gl);
-    }
+            // philo code don't touch now
+                                                player1 = new Player(1, 1, true, new String[]{"flag//flag animation1.png", "flag//flag animation2.png", "flag//flag animation3.png", "flag//flag animation4.png", "flag//flag animation5.png"});
+                                                entityManager.addEntity(player1);
+                                                for (Entity entity : entityManager.entities) {
+
+                                                        for (int i = 0; i < entity.texture.length; i++) {
+
+
+                                                            try {
+                                                                entity.texture[i] = TextureReader.readTexture(assetsFolderName + "//" + entity.Textures.get(i), true);
+                                                                gl.glBindTexture(GL.GL_TEXTURE_2D, entity.textures[i]);
+
+                                                                //                mipmapsFromPNG(gl, new GLU(), texture[i]);
+                                                                new GLU().gluBuild2DMipmaps(
+                                                                        GL.GL_TEXTURE_2D,
+                                                                        GL.GL_RGBA, // Internal Texel Format,
+                                                                        entity.texture[i].getWidth(), entity.texture[i].getHeight(),
+                                                                        GL.GL_RGBA, // External format from image,
+                                                                        GL.GL_UNSIGNED_BYTE,
+                                                                        entity.texture[i].getPixels() // Image_data
+                                                                );
+                                                            } catch (IOException e) {
+                                                                System.out.println(e.getMessage());
+                                                                e.printStackTrace();
+                                                            }
+                                                        }
+                                                    }
+            // end of philo code
+
+
+
+        }
+
 
     @Override
     public void display(GLAutoDrawable glAutoDrawable) {
@@ -64,9 +92,28 @@ public class eventListener  extends AnimListener implements GLEventListener{
         gl.glLoadIdentity();
         entityManager.update();
         entityManager.render(gl);
+        DrawBackground(gl);
 
     }
     public void DrawBackground(GL gl){
+        gl.glEnable(GL.GL_BLEND);
+        gl.glBindTexture(GL.GL_TEXTURE_2D, textures[frame]);	// Turn Blending On
+
+        gl.glPushMatrix();
+        gl.glBegin(GL.GL_QUADS);
+        // Front Face
+        gl.glTexCoord2f(0.0f, 0.0f);
+        gl.glVertex3f(-1.0f, -1.0f, -1.0f);
+        gl.glTexCoord2f(1.0f, 0.0f);
+        gl.glVertex3f(1.0f, -1.0f, -1.0f);
+        gl.glTexCoord2f(1.0f, 1.0f);
+        gl.glVertex3f(1.0f, 1.0f, -1.0f);
+        gl.glTexCoord2f(0.0f, 1.0f);
+        gl.glVertex3f(-1.0f, 1.0f, -1.0f);
+        gl.glEnd();
+        gl.glPopMatrix();
+
+        gl.glDisable(GL.GL_BLEND);
     }    @Override
     public void reshape(GLAutoDrawable glAutoDrawable, int i, int i1, int i2, int i3) {
 
